717072c55fd6c41f03b7ad8c73306da0684b55fc
buzzz321 <anders.olme@gmail.com>
2015-05-09 10:00:09 +0200

    kind of working

--- lib/dedupman.ex
+++ lib/dedupman.ex
@@ -7,6 +7,7 @@ defmodule DeDupman do
   def get_files(path) do
     Path.wildcard(path <> "/**/*.jpg")
   end
+
   #----------------------------------------------------
   def put_in_map([], utdata) do
     utdata
@@ -31,7 +32,9 @@ defmodule DeDupman do
     res = System.cmd("exiv2" , ["-g", "Pentax.LensType",  filename])
 
     if (tuple_size(res) > 1 && String.length(elem(res,0)) > 59) do
-      elem String.split_at(elem(res,0),60), 1
+      String.strip(elem(String.split_at(elem(res,0),60), 1))
+    else
+      ""
     end
   end
 
@@ -46,18 +49,37 @@ defmodule DeDupman do
       end
     end
   end 
-  #----------------------------------------------------
 
+  #----------------------------------------------------
+  def check_for_lens(tuple) do
+
+    f1 = get_lens_info(hd(elem(tuple, 1)) <> "/" <> elem(tuple,0))
+    f2 = get_lens_info(hd(tl(elem(tuple, 1))) <> "/" <> elem(tuple,0))
+
+    if (f1 == "" && String.match?(f2, ~r"smc")) do
+      #IO.puts "[#{f1}]"
+      IO.puts hd(elem(tuple, 1)) <> "/" <> elem(tuple,0)
+    end
+
+    if (f2 == "" && String.match?(f1, ~r"smc")) do
+      #IO.puts "[#{f2}]"
+      IO.puts hd(tl(elem(tuple, 1))) <> "/" <> elem(tuple,0)
+    end
+  end
+
+  #----------------------------------------------------
   def main(_args) do
     db = put_in_map(get_files("/home/anders/Pictures"), HashDict.new)
-    IO.inspect db
+    #IO.inspect db
 
     dups = get_duplicates(db) 
 
-    IO.inspect dups
-    IO.puts length dups
+    check_for_lens({"IMGP0792.jpg",["/home/anders/Pictures/2009/10/17", "/home/anders/Pictures/tekanna"]})
+    #IO.inspect dups
+    #IO.puts length dups
 
-    IO.inspect get_lens_info("/home/anders/Pictures/Tysklandssemeter/IMGP1016.jpg")
+    Enum.map(dups, &check_for_lens/1)
+    #IO.inspect get_lens_info("/home/anders/Pictures/Tysklandssemeter/IMGP1016.jpg")
     IO.puts "done"
   end
 end
